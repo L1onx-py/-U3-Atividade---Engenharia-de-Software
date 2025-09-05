45c907d893324ec931fee4f8a10ac968351e08c4
gabriel.nunes <gabriel.nunes@dcx.ufpb.br>
2025-09-05 10:22:23 -0300

    Código simples para printar frase

---
+++ scr/src/GabrielNunes.java
@@ -0,0 +1,17 @@
+public class GabrielNunes {
+    String frase;
+
+    public GabrielNunes(String frase) {
+        this.frase = frase;
+
+        System.out.print(frase);
+    }
+
+    public String getFrase() {
+        return frase;
+    }
+
+    public void setFrase(String frase) {
+        this.frase = frase;
+    }
+}
